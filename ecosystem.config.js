46867dc3a35fb71a3d2468a008a2e3b988958b93
Ian Ramirez-España <ianespana@users.noreply.github.com>
2024-12-01 17:43:11 -0600

    3er parcial

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [
+    {
+      name: "portfolio", // Name of your application
+      script: "npm",
+      args: "run start", // Replace 'start' with your NPM script name
+    },
+  ],
+};
